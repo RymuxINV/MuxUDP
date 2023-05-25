e4a8b8b6901b3f6ce54fbe4018915976f789988a
RymuxINV <134512420+RymuxINV@users.noreply.github.com>
2023-05-25 20:06:06 +0700

    Add files via upload

---
+++ UDP.py
@@ -0,0 +1,26 @@
+import socket
+
+ip = str(input('[+] Target: '))
+
+Target_host = "127.0.0.0"
+Target_port = 9999
+
+While True:
+        Try:
+            s = socket.socket(socket.AF_INET, socket.SOCKET_DGRAM)
+            s.connect((ip,port))
+            s.send(hh)
+            for i in range(pack):
+                s.send(hh)
+            x += 1
+            print (Attacking  '+ip' | Sent '+str(xx))
+
+client = socket.socket(socket.AF_INET, socket.SOCKET,DGRAM) #UDP
+
+client.sendto("Tes Aja Kok"),(target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print ("Pesan Dari Server : \*()\**,format.(data.decode))))
+
+client.close
